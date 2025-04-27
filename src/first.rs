16f1b97c6539d1f626e31b3a70ff0e2a0e0d21a0
Riki Itokazu <itokazuriki@gmail.com>
2025-04-27 14:02:47 -0400

    [bad] push pop

---
+++ src/first.rs
@@ -0,0 +1,43 @@
+use std::mem;
+
+pub struct List {
+    head: Link,
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new() -> Self{
+        // Link :: Empty refers to VARIANT of Link because it is an enum
+        List { head: Link::Empty }
+    }
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+    }
+    pub fn pop(&mut self) -> Option<i32> {
+        let res;
+        match mem::replace(&mut self.head, Link:: Empty) {
+            Link::Empty => {
+                res = None
+            }
+            Link::More(node) => {
+                res = Some(node.elem);
+                self.head = node.next;
+            }
+        };
+        res
+    }
+}
