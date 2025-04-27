cacab62f2b0e5b6b40c62b94340ee621c74894c4
Riki Itokazu <itokazuriki@gmail.com>
2025-04-27 15:45:16 -0400

    [ok] add generics

--- src/second.rs
+++ src/second.rs
@@ -1,25 +1,25 @@
 use std::mem;
 
-pub struct List {
-    head: Link,
+pub struct List<T> {
+    head: Link<T>,
 }
 
 
 // [question] type vs enum
 // Box ensures heap allocation
-type Link = Option<Box<Node>>;
+type Link<T> = Option<Box<Node<T>>>;
 
-struct Node {
-    elem: i32,
-    next: Link,
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
 }
 
-impl List {
+impl<T> List<T> {
     pub fn new() -> Self{
         // Link :: Empty refers to VARIANT of Link because it is an enum
         List { head: None }
     }
-    pub fn push(&mut self, elem: i32) {
+    pub fn push(&mut self, elem: T) {
         let new_node = Box::new(Node {
             elem: elem,
             next: self.head.take(),
@@ -27,7 +27,7 @@ impl List {
 
         self.head = Some(new_node);
     }
-    pub fn pop(&mut self) -> Option<i32> {
+    pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|node| {
             self.head = node.next;
             node.elem
@@ -38,7 +38,7 @@ impl List {
 // Drop trait is built-in rust, but to avoid recursion, we can 
 // impelemtn it ourselves for the List
 // [question] traits vs variants 
-impl Drop for List {
+impl<T> Drop for List<T> {
     fn drop(&mut self) {
         // mem::replcae returns self.head before replace
         let mut curr_node = mem::replace(&mut self.head, None);
